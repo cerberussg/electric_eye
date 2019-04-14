e600a5c1e7a9fbde601c527feb207f7c78a3e6d1
cerberussg <scott.goyette@rubypythmore.com>
2019-04-14 11:57:44 -0400

    admin check in place

--- electric_eye.py
+++ electric_eye.py
@@ -1,10 +1,23 @@
 #!/usr/local/bin python3
 
+import os
 import scapy.all as scapy
+from pyfiglet import Figlet
+
+
+def admin_check():
+    # known good for MacOS, need to test Linux and Win32 will have its own.
+    uid = os.getuid()
+    if uid != 0:
+        print('[-] Not and admin. Please run this command elevated using sudo.')
+        exit()
 
 
 def scan(ip):
     scapy.arping(ip)
 
 
+admin_check()
+custom_font = Figlet(font='cybermedium')
+print(custom_font.renderText("Electric Eye"))
 scan("192.168.1.1/24")
